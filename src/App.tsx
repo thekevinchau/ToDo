d2857c9fc9043e3bf0e1ee85545149625713f2ab
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-04-08 23:45:43 -0400

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className='flex flex-col justify-center items-center h-screen w-screen bg-gray-600'>
-      <p className="text-red-600">Hello World</p>
+      <p className="text-red-600">Hello World Update</p>
     </div>
   )
 }
