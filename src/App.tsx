723b10acbe7715c7fe0c0b11e4dff17891967363
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-04-08 23:41:05 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col justify-center items-center h-screen w-screen bg-gray-600'>
+      <p className="text-red-600">Hello World</p>
+    </div>
+  )
+}
+
+export default App
