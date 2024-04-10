584a5dce5442c1b7d5a2fc5c27b5256fd4617f5c
KEVIN CHAU <kevinch@udel.edu>
2024-04-09 20:29:31 -0400

    Conditionally rendering background color based on completion status

--- src/components/ToDoStorage.tsx
+++ src/components/ToDoStorage.tsx
@@ -4,30 +4,32 @@ import { ToDoTask } from "./ToDoTask.tsx";
 interface Task{
     taskID: number,
     taskDesc: string,
+    completed: boolean
 }
 
 export function ToDoStorage(){
     const [taskArr, setTaskArr] = useState<Task[]>([]);
     const [taskNum, setTaskNum] = useState<number>(1);
-    const [taskName, setTaskName] = useState<string>("Wash Dishes");
+    const [taskName, setTaskName] = useState<string>("");
 
     function changeTaskName(event: React.ChangeEvent<HTMLInputElement>){
         setTaskName(event.target.value);
     }
 
-    function addTask(taskDescription: string, taskNumber: number){
-        const task = { taskDesc: taskDescription, taskID: taskNumber}
+    function addTask(taskDescription: string, taskNumber: number, completed: boolean){
+        const task = { taskDesc: taskDescription, taskID: taskNumber, completed: completed }
         setTaskArr([...taskArr, task])
         setTaskNum(taskNum + 1);
+        setTaskName("");
     }
 
 
-    return <div className="border w-1/4 h-1/2 overflow-y-scroll bg-blue-950 flex flex-col items-center" >
+    return <div className="border w-5/12 h-4/6 overflow-y-scroll bg-blue-950 flex flex-col items-center" >
         <h1 className="text-white bold text-3xl mt-4 mb-2">What Needs To Be Done?</h1>
         <div className="flex justify-center items-center">
-            <input type="text" placeholder="What is your task today?" onChange={changeTaskName}></input>
-            <button className="text-red-400 border border-red-600 ml-2" onClick={() => addTask(taskName, taskNum)}>Add Task</button>
+            <input type="text" placeholder="What is your task today?" onChange={changeTaskName} className="w-max" defaultValue={taskName}></input>
+            <button className="text-red-400 border border-red-600 ml-1" onClick={() => addTask(taskName, taskNum, false)} disabled={taskName === "" ? true : false}>Add Task</button>
         </div>
-        {taskArr.map((task) => <ToDoTask taskDesc={task.taskDesc} taskID={task.taskID}></ToDoTask>)}
+        {taskArr.map((task) => <ToDoTask taskDesc={task.taskDesc} taskID={task.taskID} completed={true}></ToDoTask>)}
     </div>
 }
--- src/components/ToDoTask.tsx
+++ src/components/ToDoTask.tsx
@@ -3,11 +3,12 @@
 interface TaskProps{
     taskID: number,
     taskDesc: string,
+    completed: boolean
 }
 
-export function ToDoTask({taskID, taskDesc}: TaskProps){
+export function ToDoTask({taskID, taskDesc, completed}: TaskProps){
     //finished rendering task. Need to take input for tasks
-    return <div>
+    return <div className="border w-11/12 h-12 mt-5 mb-1" style={ completed ? {backgroundColor: "green"} : {backgroundColor: "orange"} }>
         <span>Task {taskID}: </span>
         <span>{taskDesc}</span>
     </div>
